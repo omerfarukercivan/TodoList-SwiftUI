8a3f888bbd968b837132d9fa8626a2b01ba94ec0
Ömer Faruk Ercivan <ofar38@gmail.com>
2023-10-23 14:39:30 +0300

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Ömer Faruk Ercivan on 23.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
